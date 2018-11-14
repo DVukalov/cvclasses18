54f15e3dd40ac1e99120925cf09c27eb092860c2
DVukalov <dmitrij.vukalov@yandex.ru>
2018-11-14 16:38:27 +0300

    Metrics

---
+++ demo_feature_descriptor.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+# DRAFT
+
+import os
+import sys
+
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_hamming(a, b):
+	d = 0;
+	for i in range(len(a[0])):
+		d+=bin(a[0][i] ^ b[0][i]).count("1")
+	return d	
+
+def get_distances(values, cols, flag):
+  d = []
+
+  dist = (lambda i, j : values[i] ^ values[j]) if type(values[0] is float) else (lambda x : bin(values[i] ^ values[j]).count("1"))
+  descr = []
+  for i in range(0, int(len(values) / cols)):
+    descr.append([values[i * cols : (i + 1) * cols]])
+# добавить расстояние хэмминга
+  if  flag==0:
+    for i in range(0, len(descr)):
+	    for j in range(i+1, len(descr)):
+		    d.append(get_hamming(descr[i], descr[j]))
+			#d.append(np.linalg.norm(np.array(descr[i]) - np.array(descr[j])))
+  else:
+    for i in range(0, len(descr)):
+	    for j in range(i+1, len(descr)):
+			#d.append(get_hamming(descr[i], descr[j]))
+		    d.append(np.linalg.norm(np.array(descr[i]) - np.array(descr[j])))
+  
+  return d
+  
+def compare_descriptors(filename):
+  data = dict()
+  with open(filename, 'r+', encoding=('UTF-8')) as f:
+    data = json.loads(f.read())
+  hist_data = dict()
+  i= 0;
+  for key, val in data.items():
+    hist_data[key] = get_distances(val["data"], val["cols"], i)
+    i=i+1
+
+
+  for key, val in hist_data.items():
+    val = (val - np.min(val))/np.ptp(val)
+    bins = np.linspace(min(val), max(val), 50)
+    plt.hist(val, alpha=0.5, label=key, normed=True)
+
+  plt.legend()
+  plt.show()
+
+if __name__ == "__main__":
+  if len(sys.argv) == 2:
+    compare_descriptors(sys.argv[1])
